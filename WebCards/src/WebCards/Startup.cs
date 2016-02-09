1338cd5682e4a188a419b02e261f4453e7dceb7b
Jeff Hughes <me@jeffhughes.com>
2016-02-09 05:31:36 -0600

    add CORS

--- WebCards/src/WebCards/Startup.cs
+++ WebCards/src/WebCards/Startup.cs
@@ -15,11 +15,17 @@ namespace WebCards
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+                           options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+                                                               .AllowAnyMethod()
+                                                               .AllowAnyHeader()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseCors("AllowAll");
+
             app.UseIISPlatformHandler();
 
             app.UseDefaultFiles();
